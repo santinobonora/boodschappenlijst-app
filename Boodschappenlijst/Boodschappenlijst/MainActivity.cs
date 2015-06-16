2d4aa9898ec728690373cbb6ff22eb01e94b350c
joeykieboom <joey.kieboom@home.nl>
2015-06-16 15:40:13 +0200

    de hele app toegevoegd

---
+++ Boodschappenlijst/Boodschappenlijst/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Boodschappenlijst
+{
+	[Activity (Label = "Boodschappenlijst", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button btnRegister = FindViewById<Button> (Resource.Id.btnRegister);
+
+			btnRegister.Click += (object sender, EventArgs e) => {
+				AlertDialog.Builder alert = new AlertDialog.Builder(this);
+				alert.SetMessage("Hallo hiero");
+				alert.Show();
+			};
+		}
+	}
+}
+
+
